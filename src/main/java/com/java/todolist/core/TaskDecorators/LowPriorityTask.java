2576787163195533a0cf413fac20c924e471f3d7
bassaoud <mohamed.bassaoud@yahoo.fr>
2023-01-14 20:45:27 +0100

    Projet client lourd

---
+++ src/main/java/com/java/todolist/core/TaskDecorators/LowPriorityTask.java
@@ -0,0 +1,20 @@
+package com.java.todolist.core.TaskDecorators;
+
+import com.java.todolist.core.PRIORITY;
+import com.java.todolist.core.Task;
+import lombok.Getter;
+
+public class LowPriorityTask extends TaskDecorator {
+    @Getter
+    private PRIORITY priority;
+
+    public LowPriorityTask(Task task){
+        super(task);
+        this.priority = PRIORITY.LOW;
+    }
+    
+    @Override
+    public PRIORITY getPriority() {
+        return this.priority;
+    }
+}
---
+++ src/main/java/com/java/todolist/core/TaskDecorators/NormalPriorityTask.java
@@ -0,0 +1,20 @@
+package com.java.todolist.core.TaskDecorators;
+
+import com.java.todolist.core.PRIORITY;
+import com.java.todolist.core.Task;
+import lombok.Getter;
+
+public class NormalPriorityTask extends TaskDecorator {
+    @Getter
+    private PRIORITY priority;
+
+    public NormalPriorityTask(Task task){
+        super(task);
+        this.priority = PRIORITY.NORMAL;
+    }
+    
+    @Override
+    public PRIORITY getPriority() {
+        return this.priority;
+    }
+}
